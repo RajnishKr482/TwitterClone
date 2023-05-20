13b481b0cbecbe951c7eeab2bbe3937c71fa306a
RajnishKr482 <rajnish16303@gmail.com>
2023-05-20 17:41:37 +0530

    new tweet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
 import { FlatList, StyleSheet, View } from "react-native";
 import tweets from "../../assets/data/tweets";
 import Tweet from "../../components/Tweet";
-
+import { Entypo } from "@expo/vector-icons";
+import { Link } from "expo-router";
 export default function TabOneScreen() {
   return (
     <View style={styles.page}>
@@ -9,6 +10,14 @@ export default function TabOneScreen() {
         data={tweets}
         renderItem={({ item }) => <Tweet tweet={item} />}
       />
+      <Link href="/new-tweet" asChild>
+        <Entypo
+          name="plus"
+          size={24}
+          color="white"
+          style={styles.floatingButton}
+        />
+      </Link>
     </View>
   );
 }
@@ -17,5 +26,27 @@ const styles = StyleSheet.create({
   page: {
     backgroundColor: "white",
     flex: 1,
+  },
+  floatingButton: {
+    backgroundColor: "#1C9BF0",
+
+    borderRadius: 25,
+    padding: 15,
+
+    position: "absolute",
+    right: 15,
+    bottom: 15,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+
+    overflow: "hidden",
   },
 });
