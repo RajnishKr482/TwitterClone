1ebc246c96c3c775e5a692def6cc1a2f246db4b2
RajnishKr482 <rajnish16303@gmail.com>
2023-05-28 16:25:14 +0530

    drawer

--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { FlatList, StyleSheet, View } from "react-native";
-import tweets from "../../assets/data/tweets";
-import Tweet from "../../components/Tweet";
+import tweets from "../../../assets/data/tweets";
+import Tweet from "../../../components/Tweet";
 import { Entypo } from "@expo/vector-icons";
 import { Link } from "expo-router";
 export default function TabOneScreen() {
